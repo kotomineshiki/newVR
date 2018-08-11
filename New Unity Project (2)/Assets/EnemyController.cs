306d8b7feff7202090d3fa2bb08d1228e2f60f51
kotomineshiki <kotomineshiki@outlook.com>
2018-08-11 21:50:29 +0800

    te

--- New Unity Project (2)/Assets/EnemyController.cs
+++ New Unity Project (2)/Assets/EnemyController.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyState
+{
+    Idle,
+    Moving,
+    Attacking,
+    Dead
+}
 public class EnemyController : MonoBehaviour {
     //用来控制敌人的指令器
+    public EnemyState enemyState=EnemyState.Idle;
     public GameObject enemy;
+    public float speed = 10;
+    public GameObject toFollow;
+    public Vector3 Destination;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,21 +23,35 @@ public class EnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Fire1"))
+        Vector3 delta = toFollow.transform.position - enemy.transform.position;
+        if (RhythmController.instance.GetCurrentState() == RhythmState.Instruction)//指令时刻
+        {
+            if (enemyState == EnemyState.Idle)
+            {
+
+            }
+        }
+        if (RhythmController.instance.GetCurrentState() == RhythmState.Action)//只会在action时段执行动作
+        {
+            if (enemyState == EnemyState.Moving)
+            {
+                this.transform.position += Time.deltaTime * speed;
+            }
+        }
+
+        if (delta.magnitude < 1f)//距离充分近，可以发动攻击了
         {
             Attack();
         }
-        if (Input.GetButtonDown("Fire2"))
-        {
-            MoveToward();
-        }
 	}
+
     void Attack()
     {
         enemy.GetComponent<Animator>().SetTrigger("Attack_trigger");//播放攻击动画
     }
-    void MoveToward()
+    void MoveToward(Vector3 input)
     {
+        Destination = input;
         enemy.GetComponent<Animator>().SetTrigger("Idle_Walk_trigger");//播放移动的动画
     }
 }
